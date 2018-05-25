52df1efd8868e19bfeb5effd3f0d3651deed1dab
Maxim Bonnaerens <maxim@bonnaerens.be>
2018-05-25 12:17:23 +0200

    Added most_active_chat functionality

--- insights/messages.py
+++ insights/messages.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 
-from insights.utils import m2hm
+from insights.utils import m2hm, get_colors
 from matplotlib.dates import DayLocator, DateFormatter
 from matplotlib.ticker import FuncFormatter, MultipleLocator
 import matplotlib.pyplot as plt
@@ -228,3 +228,113 @@ def message_activity_weekly(data_path, sender, show=False):
     if show:
         plt.show()
     plt.close()
+
+
+def most_active_chat(data_path, tick_width=1, end_date=None, start_date=30, legend="text", show=False):
+    """
+    Plot the most active chat for each day between start and end date.
+    Dates on the X-axis, number of messages on the y-axis, and the specific chat is encoded in the color of the bars.
+
+    Parameters
+    ----------
+    data_path : str
+        Path to the root directory of your personal facebook data.
+    tick_width : float
+        width in inches between consecutive ticks on both axis.
+    end_date : datetime-like or None, optional
+        Last date to include in analysis.
+    start_date : datetime-like or int, optional
+        If datetime-like: first date to include in analysis.
+        If int: Start date will be X days before end_date
+    legend : {"text", "box", None}
+        Style for legend.
+    show : bool, default to False
+        Show the generated plot.
+
+    """
+    path = os.path.join(data_path, "messages")
+    directories = os.listdir(path)
+    df = None
+    for directory in directories:
+        directory_path = os.path.join(path, directory)
+        if os.path.isdir(directory_path) and directory not in ["insights", "stickers_used"]:
+            csv_path = os.path.join(directory_path, "message.csv")
+            if os.path.isfile(csv_path):
+                chat = directory.split('_')[0]
+                new_df = pd.read_csv(csv_path)
+                new_df['chat'] = chat
+                if df is None:
+                    df = new_df
+                else:
+                    df = df.append(new_df, ignore_index=True)
+    df['time'] = pd.to_datetime(df['time'], format="%d %B %Y %H:%M")
+    df['date'] = df['time'].dt.normalize()
+    df = df.groupby(['date', 'chat']).size().reset_index(name='amount_messages')
+    idx = df.groupby(['date'])['amount_messages'].transform(max) == df['amount_messages']
+    df = df[idx].drop_duplicates(subset=['date', 'amount_messages'])
+
+    last_date = df['date'].max()
+    if end_date is None:
+        end_date = last_date
+    else:
+        end_date = pd.Timestamp(end_date)
+        end_date = min(end_date, last_date)
+
+    if isinstance(start_date, int):
+        days = start_date
+        start_date = max(df['date'].min(), end_date - datetime.timedelta(days))
+    else:
+        start_date = pd.Timestamp(start_date)
+
+    df = df[df['date'] <= end_date]
+    df = df[df['date'] >= start_date]
+
+    chats = df.chat.unique()
+    colors = get_colors(len(chats))
+
+    fig, ax = plt.subplots(1, 1)
+
+    for i, chat in enumerate(chats):
+        chat_df = df[df.chat == chat]
+        ax.bar(chat_df['date'].tolist(), chat_df['amount_messages'], color=colors[i], alpha=0.8, label=chat)
+
+    if legend == "text":
+        for x, value in zip(df['date'].tolist(), df['chat']):
+            ax.text(x, len(value) + ax.get_ylim()[1] / 10, value, rotation=90, size=14)
+    if legend == "box":
+        ax.legend()
+
+
+    ax.xaxis_date()
+
+    major_locator = MultipleLocator(10)
+    ax.yaxis.set_major_locator(major_locator)
+    ax.yaxis.grid(linestyle=':', linewidth=0.5)
+    y_length = max(ax.get_ylim()[1] / 20 * tick_width, 10)
+
+
+    period = (end_date - start_date).days
+    if period > 366:
+        interval = 7
+    else:
+        interval = 1
+    ax.xaxis.set_major_locator(DayLocator(interval=interval))
+    ax.xaxis.set_major_formatter(DateFormatter('%d/%m/%Y'))
+    ax.xaxis.grid(linestyle=':', linewidth=0.5)
+    x_length = max(period * tick_width, 5)
+    fig.autofmt_xdate(rotation=90)
+    fig.set_size_inches(x_length, y_length)
+
+    output_path = os.path.join(path, "insights")
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    file_name = "most_active_chat.png"
+    file_name = os.path.join(output_path, file_name)
+    plt.savefig(file_name, bbox_inches='tight')
+
+    if show:
+        plt.show()
+    plt.close()
+
+    return
--- insights/utils.py
+++ insights/utils.py
@@ -1,3 +1,8 @@
+import colorsys
+
+def get_colors(N=5):
+    return  [colorsys.hsv_to_rgb(x*1.0/N, 0.5, 0.5) for x in range(N)]
+
 def m2hm(x, i):
     h = int(x/60)
     m = int(x%60)
